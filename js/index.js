878dc6f85f9b983f02c01a1eb44776dc8cb103a7
JIH-L <sol@livemail.tw>
2020-10-17 03:05:08 +0800

    Update main

---
+++ js/index.js
@@ -0,0 +1,28 @@
+
+//navbar
+
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll',()=>{
+    const scrollPos = window.scrollY;
+    if(scrollPos>10){
+        nav.classList.add('scrolled');
+    }
+    else{
+        nav.classList.remove('scrolled');
+
+    }
+})
+
+// 打字機效果
+const dom = document.querySelector('.content'); 
+console.log(dom);
+const data = 'Hello, Github.io, 你好.'.split('') ;
+let index = 0 ;
+function writing(index) { 
+    if (index < data.length) {
+         dom.innerHTML += data[index] 
+         setTimeout(writing.bind(this), 200, ++index) 
+        } 
+};
+writing(0);
