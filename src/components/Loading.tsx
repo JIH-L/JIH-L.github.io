7e78162d7f9259c8cab222051c82a643ae0b4c67
JIH L <sol@livemail.tw>
2023-09-13 10:55:52 +0800

    feat: loading animation

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,11 +1,7 @@
 export default function Loading() {
   return (
-    <div className="z-50 flex h-screen items-center justify-center bg-gray-300">
-      <img
-        src="https://i.imgur.com/0HSGxif.gif"
-        alt=""
-        className="h-[150px] w-[150px] "
-      />
+    <div className="z-50 flex h-screen items-center justify-center bg-gray-200 dark:bg-neutral-800">
+      <div className="h-[30px] w-[30px] animate-spin rounded-[100%] border-2 border-solid border-black border-t-transparent dark:border-white dark:border-t-neutral-800"></div>
     </div>
   )
 }
