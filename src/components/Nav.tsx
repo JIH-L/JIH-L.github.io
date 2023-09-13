7132f66fd027017c797e6d10923f6f4ce3f2d9af
JIH L <sol@livemail.tw>
2023-09-13 16:29:28 +0800

    feat: update nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,21 @@
 import { NavLink } from 'react-router-dom'
 import ThemeSwitcher from '../components/ThemeSwitcher'
+function ALink({ label, href }: { label: string; href: string }) {
+  return (
+    <a
+      href={href}
+      className="rounded-lg p-3 text-sm duration-500 hover:bg-gray-100 dark:hover:bg-neutral-700"
+    >
+      {label}
+    </a>
+  )
+}
+const links = [
+  { href: '/#about', label: 'About' },
+  { href: '/#skill', label: 'Skill' },
+  { href: '/#project', label: 'Project' },
+  { href: '/#contact', label: 'Contact' },
+]
 export default function Nav() {
   return (
     <nav className="sticky top-0 z-40 flex items-center bg-white p-6 shadow-md dark:bg-neutral-800 dark:text-white">
@@ -7,38 +23,9 @@ export default function Nav() {
         J.L.
       </NavLink>
       <ul className="mr-2 flex">
-        <li className="mr-3">
-          <a
-            href="/#about"
-            className="rounded-lg p-3 text-sm duration-500 hover:bg-gray-100"
-          >
-            About
-          </a>
-        </li>
-        <li className="mr-3">
-          <a
-            href="/#skill"
-            className="rounded-lg p-3 text-sm duration-500 hover:bg-gray-100"
-          >
-            Skill
-          </a>
-        </li>
-        <li className="mr-3">
-          <a
-            href="/#project"
-            className="rounded-lg p-3 text-sm duration-500 hover:bg-gray-100"
-          >
-            Project
-          </a>
-        </li>
-        <li>
-          <a
-            href="/#contact"
-            className="rounded-lg p-3 text-sm duration-500 hover:bg-gray-100"
-          >
-            Contact
-          </a>
-        </li>
+        {links.map((link) => (
+          <ALink key={link.href} href={link.href} label={link.label} />
+        ))}
       </ul>
       <ThemeSwitcher />
     </nav>
