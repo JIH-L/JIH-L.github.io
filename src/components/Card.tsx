b5b732182eb4ed8df7f3a671a40cc40ef36b0253
JIH L <sol@livemail.tw>
2023-09-13 16:29:57 +0800

    feat: update card component dark mode

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,10 +8,12 @@ interface CardProps {
 }
 export default function Card({ title, description, image, link }: CardProps) {
   return (
-    <div className=" cursor-pointer rounded-lg bg-gray-50 p-4 pb-6 duration-300 hover:bg-gray-100">
+    <div className=" cursor-pointer rounded-lg bg-gray-50 p-4 pb-6 duration-300 hover:bg-gray-100 dark:bg-neutral-700 dark:hover:bg-neutral-600">
       <img src={image} alt="" className="mb-4 w-full rounded-lg" />
-      <h4 className="mb-4 text-xl font-semibold">{title}</h4>
-      <p className="mb-4 text-sm text-gray-500">{description}</p>
+      <h4 className="mb-4 text-xl font-semibold dark:text-white">{title}</h4>
+      <p className="mb-4 text-sm text-gray-500 dark:text-gray-300">
+        {description}
+      </p>
       <NavLink
         to={link}
         className="inline-flex items-center rounded-md border border-black bg-black px-4 py-2 text-xs text-white duration-200 hover:scale-105"
