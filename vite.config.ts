c4e31ae02432cf2305a4cf529adb817878392b7e
JIH-L <sol@livemail.tw>
2025-03-07 17:38:04 +0800

    fix: remove dynamic base path configuration in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
-    base: mode === 'production' ? '/JIH-L.github.io/' : '/',
+    base: '/',
     plugins: [react()],
   }
 })
